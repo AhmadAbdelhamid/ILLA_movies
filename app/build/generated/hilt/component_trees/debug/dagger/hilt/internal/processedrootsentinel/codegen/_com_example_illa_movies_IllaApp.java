a4c8444f41ff40ce989c0d2d2a3e5fb73c48aeb1
ahmed <ahmad.abdelhamid@gmail.com>
2023-03-07 01:37:01 +0200

    remote repo created

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_illa_movies_IllaApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.example.illa_movies.IllaApp"
+)
+public final class _com_example_illa_movies_IllaApp {
+}
---
+++ app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_illa_movies_IllaApp.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.illa_movies.IllaApp",
+    rootPackage = "com.example.illa_movies",
+    originatingRoot = "com.example.illa_movies.IllaApp",
+    originatingRootPackage = "com.example.illa_movies",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "IllaApp",
+    originatingRootSimpleNames = "IllaApp"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_example_illa_movies_IllaApp {
+}
